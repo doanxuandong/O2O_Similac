a98bb7534fabe81abdb630c8705c3b27beb715a3
doanxuandong <doanxuandong8303@gmail.com>
2025-07-25 23:49:57 +0700

    done-1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import bgrMobileJpg from './Images/bgrmb.png'
 import bigcLogo from './Images/bigc.png'
 import coopLogo from './Images/coop.png'
 import gtLogo from './Images/GT.png'
+import milk1 from './Images/element-19.png'
+import milk2 from './Images/element-21.png'
+import rose from './Images/rose.png'
 import './App.css'
 
 function App() {
@@ -29,6 +32,15 @@ function App() {
         className="block sm:hidden w-full object-contain z-[1] pointer-events-none select-none"
         style={{ position: 'relative' }}
       /> */}
+      {/* 2 lon sữa dưới đáy mobile */}
+      <div className="block sm:hidden absolute bottom-0 left-0 w-full flex justify-center z-10">
+        <img src={milk1} alt="Sữa 1" className="w-24 h-auto mx-2 transform transition-transform duration-300 hover:scale-110" />
+        <img src={milk2} alt="Sữa 2" className="w-24 h-auto mx-2 transform transition-transform duration-300 hover:scale-110" />
+      </div>
+      {/* Hoa hồng góc trái dưới mobile */}
+      <div className="block sm:hidden absolute bottom-0 left-0 z-0">
+        <img src={rose} alt="Rose" className="w-300 h-auto opacity-80" />
+      </div>
       <div className="relative w-full h-full">
         {/* Hiệu ứng mờ 2 bên ảnh nền */}
         {/* <div className="absolute top-0 left-0 w-[10vw] h-full z-10 pointer-events-none hidden sm:block" style={{background: 'linear-gradient(to right, #fffbe7 65%, transparent)'}} />
@@ -40,8 +52,8 @@ function App() {
           className="absolute top-0 left-0 w-full h-full object-contain z-[1] pointer-events-none select-none hidden sm:block"
         />
         {/* Table 3 ô logo - MOBILE */}
-        <div className="relative mt-4 w-full flex justify-center sm:absolute sm:left-[67%] sm:-translate-x-1/2 sm:bottom-[15%] sm:z-10 sm:mt-0">
-          <table className="block sm:hidden bg-transparent border-separate border-spacing-4">
+        <div className="relative w-full flex justify-center sm:absolute sm:left-[67%] sm:-translate-x-1/2 sm:bottom-[15%] sm:z-10 sm:mt-0">
+          <table className="block sm:hidden bg-transparent border-separate border-spacing-4 mt-80 sm:mt-0">
             <tbody>
               <tr>
                 <td colSpan="3" className="pb-2 text-center">
@@ -68,7 +80,7 @@ function App() {
             </tbody>
           </table>
           {/* Table 3 ô logo - DESKTOP */}
-          <table className="hidden sm:table bg-transparent border-separate border-spacing-4 sm:border-spacing-x-10">
+          <table className="hidden sm:table bg-transparent border-separate border-spacing-4 sm:border-spacing-x-10 mt-0 ">
             <tbody>
               <tr>
                 <td colSpan="3" className="pb-2 text-center">
