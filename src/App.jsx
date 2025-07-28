1e7b7120590d9b6472e89f3e1732cba53e22f8e5
doanxuandong <doanxuandong8303@gmail.com>
2025-07-28 16:01:25 +0700

    check

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
         </div>
         
         {/* Mokup mobile */}
-        <div className="w-full flex justify-center z-10 pt-4 mt-auto">
+        <div className="w-full flex justify-center z-10 pt-4 mt-1">
           <img src={mokup} alt="Mokup" className="w-1/2 h-auto" />
         </div>
         
