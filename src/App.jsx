89ad2c4b14ac09fcaed4a6c4a6b72f6422b76c49
doanxuandong <doanxuandong8303@gmail.com>
2025-07-29 15:07:07 +0700

    update 15h 29/7

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ function App() {
             <tbody>
               <tr>
                 <td colSpan="3" className="pb-0 text-center">
-                  <span className="font-bold text-sm sm:text-2xl border-2 border-blue-900 p-1 pl-2 pr-2 rounded-[999px] bg-[#fff] text-[#315bb7]">THAM GIA NGAY TẠI CÁC ĐIỂM BÁN</span>
+                  <span className="font-bold text-sm sm:text-2xl border-2 border-blue-900 p-1 pl-2 pr-2 rounded-[999px] bg-[#154da1] text-[#fff]">THAM GIA NGAY TẠI CÁC ĐIỂM BÁN</span>
                 </td>
               </tr>
               <tr>
@@ -118,34 +118,45 @@ function App() {
         </div> */}
         
         {/* Table 3 ô logo - DESKTOP */}
-        <div className="relative w-full flex justify-center sm:absolute sm:left-[68%] sm:-translate-x-1/2 sm:bottom-[13%] sm:z-10 sm:mt-0">
-          <table className="hidden sm:table bg-transparent border-separate border-spacing-4 sm:border-spacing-x-10 mt-0">
+        <div className="relative w-full flex justify-center sm:absolute sm:left-[68%] sm:-translate-x-1/2 sm:bottom-[12%] sm:z-10 sm:mt-0">
+          <table className="hidden sm:table bg-transparent border-separate border-spacing-2 sm:border-spacing-x-5 mt-0">
             <tbody>
               <tr>
                 <td colSpan="3" className="pb-2 text-center">
-                  <span className="font-bold text-xl border-2 border-blue-900 p-1 pl-2 pr-2 rounded-[999px] bg-[#154da1] sm:text-xl text-[#fff]">NHẤN ĐỂ MUA HÀNG TẠI CÁC ĐIỂM BÁN</span>
+                  <span className="font-bold text-xl border-2 border-blue-900 p-1 pl-2 pr-2 rounded-[999px] bg-[#154da1] sm:text-sm text-[#fff]">CHỌN ĐIỂM BÁN BÊN DƯỚI ĐỂ THAM GIA CHƯƠNG TRÌNH</span>
                 </td>
               </tr>
-              <tr>
-                <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[168px] sm:h-[96px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
-                  <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
-                    {/* <img src={bigcLogo} alt="BigC" className="w-full h-full object-contain hidden sm:block" /> */}
-                    <img src={bigcLogoDesktop} alt="BigC Desktop" className="w-full h-full object-contain sm:block" />
-                  </a>
-                </td>
-                <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[168px] sm:h-[96px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
-                  <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
-                    {/* <img src={coopLogo} alt="Co.op" className="w-full h-full object-contain hidden sm:block" /> */}
-                    <img src={coopLogoDesktop} alt="Co.op Desktop" className="w-full h-full object-contain sm:block" />
-                  </a>
-                </td>
-                <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[168px] sm:h-[96px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
-                  <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
-                    {/* <img src={gtLogo} alt="GT" className="w-full h-full object-contain hidden sm:block" /> */}
-                    <img src={gtLogoDesktop} alt="GT Desktop" className="w-full h-full object-contain sm:block" />
-                  </a>
-                </td>
-              </tr>
+                              <tr>
+                  <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[140px] sm:h-[80px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
+                    <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
+                      {/* <img src={bigcLogo} alt="BigC" className="w-full h-full object-contain hidden sm:block" /> */}
+                      <img src={bigcLogoDesktop} alt="BigC Desktop" className="w-full h-full object-contain sm:block" />
+                    </a>
+                  </td>
+                  <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[140px] sm:h-[80px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
+                    <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
+                      {/* <img src={coopLogo} alt="Co.op" className="w-full h-full object-contain hidden sm:block" /> */}
+                      <img src={coopLogoDesktop} alt="Co.op Desktop" className="w-full h-full object-contain sm:block" />
+                    </a>
+                  </td>
+                  <td className="bg-gradient-to-br from-[#fffbe7] to-[#f7e09e] border-2 border-[#f7c948] rounded-[6px] shadow-lg w-24 h-24 sm:w-[140px] sm:h-[80px] align-middle text-center p-0 overflow-hidden transform transition-transform duration-300 hover:scale-110 hover:shadow-[0_0_40px_#fff]">
+                    <a href="https://zalo.me/s/886890990835804119/" target="_blank" rel="noopener noreferrer">
+                      {/* <img src={gtLogo} alt="GT" className="w-full h-full object-contain hidden sm:block" /> */}
+                      <img src={gtLogoDesktop} alt="GT Desktop" className="w-full h-full object-contain sm:block" />
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="pt-0 align-top text-center">
+                    <span className="text-xs font-medium text-[#315bb7] leading-tight -mt-1 block">Chuỗi siêu thị BigC, Go!, Tops Market</span>
+                  </td>
+                  <td className="pt-0 align-top text-center">
+                    <span className="text-xs font-medium text-[#315bb7] leading-tight -mt-1 block">Chuỗi siêu thị Co.op, Co.op Extra, Co.op Food, Finelife</span>
+                  </td>
+                  <td className="pt-0 align-top text-center">
+                    <span className="text-xs font-medium text-[#315bb7] leading-tight -mt-1 block">Hệ thống cửa hàng bán lẻ</span>
+                  </td>
+                </tr>
             </tbody>
           </table>
         </div>
