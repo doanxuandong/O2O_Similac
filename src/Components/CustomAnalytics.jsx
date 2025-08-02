3dd2c8a40289675c0e8dc1cc7224b6c0724b05aa
doanxuandong <doanxuandong8303@gmail.com>
2025-08-02 17:24:46 +0700

    fndskjfhskdfd

--- src/Components/CustomAnalytics.jsx
+++ src/Components/CustomAnalytics.jsx
@@ -1,5 +1,5 @@
 // Custom Analytics component sử dụng Google Sheets
-const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbwO_-Lb53a8N-E6jLIlpNqez_TWFsQlbiu3PC5Mu9qcu-n8kRJ91GOdOhhl1XjGV7AP/exec";
+const GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbx390vmmwr9fTs-ka2XryKk-04Vd5rs_-lvXhKCa89A5cNRIWVUkzCSHKhUBX2E6g1s/exec";
 
 export const trackPageView = (pageTitle = 'Vu Lan') => {
   const params = new URLSearchParams(window.location.search);
